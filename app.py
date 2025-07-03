b1b94a8eb4f6455d91ff661aa8de5fa21aec886e
PiotrKley259 <kleymenovpiotr@gmail.com>
2025-07-03 21:00:42 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import io
 import math
 import datetime
 from collections import defaultdict
+import os
 
 app = Flask(__name__)
 app.secret_key = "supersecret"
@@ -104,4 +105,5 @@ def investment_balance():
     return jsonify({"balance": investment_account["balance"]})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
